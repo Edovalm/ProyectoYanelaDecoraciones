1dff9e3288177ce6360b159358226ea6cbd8590d
Edovalm <edoval2680@gmail.com>
2021-09-24 20:53:47 -0500

    Se creó carpeta js y archivo main.js en el que se agregó código javaScript para gestionar ineractividad para las ventanas modales en tamaños móviles para el menú de navegación y el formulario de búsqueda, también código javaScript para dar efecto de animación a los elementos con la clase animated-element, y se linkeó el script en el archivo index.html

---
+++ Site/js/index.js
@@ -0,0 +1,29 @@
+//Function Toggle
+function toggle(idActivator,classElement,classElementActive) {
+    const activator = document.querySelector(idActivator)
+    const theElement = document.querySelector(classElement) 
+
+    activator.addEventListener("click", () => {
+        theElement.classList.toggle(classElementActive)
+    }); 
+}
+
+//Llamado de Function Toggle
+toggle("#menu-toggle",".main-nav__menu","main-nav__menu--active");
+toggle("#search-toggle",".main-nav__search","main-nav__search--active");
+
+//Elementos Animados 
+let animatedElement = document.querySelectorAll(".animated-element");
+let windowHeight = window.innerHeight;
+function animatedScroll(){
+    let scrollTop = (document.documentElement.scrollTop);
+    for(var i = 0; i < animatedElement.length; i++){
+        let heightForAnimation = animatedElement[i].offsetTop;
+        if(heightForAnimation - (windowHeight/2)  < scrollTop) {
+            animatedElement[i].style.opacity = 1;
+            animatedElement[i].classList.add("from-above");
+        }
+    }
+}
+
+window.addEventListener("scroll", animatedScroll);
